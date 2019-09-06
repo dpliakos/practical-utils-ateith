ad4543ba14452bbb8811a0767dd42be0944b9560
dpliakos <dimitrispl96@gmail.com>
2019-09-06 22:57:16 +0300

    feat(actions): Add kdiamats email replacement.

---
+++ README.md
@@ -0,0 +1,26 @@
+# Practical utils for ateith students
+
+Let's be honest. The world is great, but could be better.
+You can change the every day life of ATEITH students by solving those tiny little problems that appear every now and then.
+
+## How:
+
+1.Go to `/src/app.js` create an onject with the `action` and `condition` function.
+
+```javascript
+
+var myObject = {
+  condition: function() {
+    // your code goes here
+  }, 
+  action: function() {
+    // your code goes here
+  }
+}
+```
+
+2. Update the  `listOfActions` to contain your action.
+
+3. Write at the `/src/index.html` what the action you added does.
+
+4. Feel the glory of a real hero.
---
+++ src/app.js
@@ -0,0 +1,49 @@
+"use strict";
+
+/**
+ * 
+ * Declares a list of actions, calls an action if the given condition is true.
+ * 
+ */
+
+/**
+ * Declare your action here:
+ */
+
+ /**
+  * 
+  * Replaces the image with the kdiamant email with a proper url
+  * 
+  */
+var kdiamantEmailEeplacement = {
+  condition: function() {
+    var targetUrl = "aetos.it.teithe.gr/~kdiamant";
+    return window.location.toString().indexOf(targetUrl) >= 0;
+  },
+  action: function() {
+    var emailWrapper = document.querySelector("p[title=Email]");
+    var emailImage = document.querySelector("p[title=Email] img:nth-child(2)");
+
+    var targetElement = document.createElement("a");
+    targetElement.setAttribute("href", "mailto:kdiamant@it.teithe.gr");
+    targetElement.innerText = "kdiamant@it.teithe.gr";
+
+    emailWrapper.removeChild(emailImage);
+    emailWrapper.appendChild(targetElement);    
+  }
+}
+
+
+
+
+var listOfActions = [
+  kdiamantEmailEeplacement
+];
+
+for (var i = 0; i < listOfActions.length; i++) {
+  console.debug("1")
+  if (listOfActions[i].condition()) {
+    console.debug("2")
+    listOfActions[i].action();
+  }
+}
